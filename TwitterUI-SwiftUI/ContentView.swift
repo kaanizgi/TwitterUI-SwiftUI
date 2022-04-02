ff6c5c31b8f0b247bcd426f5908c5d0c3c4a0c7e
Kaan İzgi <kaanizgi1531@gmail.com>
2022-04-02 13:15:29 +0300

    Initial Commit

---
+++ TwitterUI-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwitterUI-SwiftUI
+//
+//  Created by Kaan İzgi on 2.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwitterUI-SwiftUI/TwitterUI_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterUI_SwiftUIApp.swift
+//  TwitterUI-SwiftUI
+//
+//  Created by Kaan İzgi on 2.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct TwitterUI_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
